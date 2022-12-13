6fef4197fd9214bc459ac40f753a4e4e6dc2d9e3
DandyCat91 <112863136+DandyCat91@users.noreply.github.com>
2022-12-13 13:08:40 +0300

    create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def func_sum(a, b, c):
+    sum = a + b * c
+    return sum
+
+x = func_sum(1, 2, 3)
+print(x)
+
+# либо
+
+a = int(input())
+b = int(input())
+c = int(input())
+x = a + b * c
+print(x)
